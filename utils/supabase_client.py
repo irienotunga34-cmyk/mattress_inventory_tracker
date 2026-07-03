5ca9580971d95eecb9388be675fff660cc38d3be
Irien Otunga <irienotunga34@gmail.com>
2026-07-03 13:57:28 +0300

    Use Streamlit secrets for Supabase

--- utils/supabase_client.py
+++ utils/supabase_client.py
@@ -1,14 +1,8 @@
 from supabase import create_client
-from dotenv import load_dotenv
-import os
+from supabase import create_client
+import streamlit as st
 
-load_dotenv()
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-response = client.table("sales").select("*").limit(1).execute()
-
-print(response.data)
