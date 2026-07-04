d8b343a0a16c991d8e9ce0de699545ab0d9efa55
Irien Otunga <irienotunga34@gmail.com>
2026-07-04 12:27:21 +0300

    Remove debug messages

--- utils/supabase_client.py
+++ utils/supabase_client.py
@@ -1,13 +1,9 @@
+
 import streamlit as st
 from supabase import create_client
 
-st.write("URL exists:", "SUPABASE_URL" in st.secrets)
-st.write("KEY exists:", "SUPABASE_KEY" in st.secrets)
+SUPABASE_URL = "https://sngspougndedjrvvjrjb.supabase.co"
 
-SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-
-st.write("URL:", SUPABASE_URL)
-st.write("Key length:", len(SUPABASE_KEY))
+SUPABASE_KEY = "sb_publishable_GTtgR0gAKEa0W82RMzufIA_DT2mpPhq"
 
 client = create_client(SUPABASE_URL, SUPABASE_KEY)
